1a3673e9f82d29ae150db7675a1510d8dc0a43e0
Blake Gideon <blake@chicagoan.io>
2019-01-03 13:03:49 -0600

    Update 3-1-actual_biased.py
    Clarified where code from textbook is

--- statistics/3-1-actual_biased.py
+++ statistics/3-1-actual_biased.py
@@ -14,7 +14,7 @@ biased_kd_count = kd_count[1:]
 print(f'Unbiased Count: \n{kd_count} \n\nBiased Count:\n {biased_kd_count}')
 
 
-
+# Copied from (Page 37 in textbook). 
 def BiasPmf(pmf, label): 
     new_pmf = pmf.Copy(label=label) 
     
@@ -24,8 +24,8 @@ def BiasPmf(pmf, label):
     new_pmf.Normalize() 
     
     return new_pmf
+# End copy
 
-# Copied from (Page 37 in textbook). 
 
 pmf = thinkstats2.Pmf(kd_count, label='Real')
 biased = BiasPmf(pmf, label='Biased')
